5a81c35531a552da9d4cf8acb8ff37627c505180
Diovanni-ls <diovanni.lucas2019@gmail.com>
2024-12-05 16:37:52 -0300

    Segunda Versão

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function insert(num){
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
+function insert(num) {
+    const resultado = document.getElementById('resultado');
+    resultado.textContent += num;
+
+    // Rola automaticamente para o final do visor
+    resultado.scrollLeft = resultado.scrollWidth;
 }
+
+function calcular() {
+    const resultado = document.getElementById('resultado');
+    try {
+        resultado.textContent = eval(resultado.textContent);
+
+        // Rola automaticamente para o final do visor
+        resultado.scrollLeft = resultado.scrollWidth;
+    } catch (error) {
+        resultado.textContent = 'Erro';
+    }
+}
+
 
 function clean(){
     document.getElementById('resultado').innerHTML = '';
@@ -10,14 +26,4 @@ function clean(){
 function back(){
     var numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero.substring(0, numero.length -1);
-}
-
-function calcular(){
-    var numero = document.getElementById('resultado').innerHTML;
-    if(resultado){
-        document.getElementById('resultado').innerHTML = eval(numero);
-    }
-    else{
-        document.getElementById('resultado').innerHTML = 'Error';
-    }
 }
